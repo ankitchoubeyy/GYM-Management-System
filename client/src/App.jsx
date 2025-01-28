01f19e3ed732afe8041bbb4958daaa5cb71c828b
ankitchoubeyy <imkitchoubey@gmail.com>
2025-01-29 00:05:59 +0530

    daisy ui, react-router-dom and navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl text-red-500'>GYM</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { CiMenuFries } from "react-icons/ci";
+import { RxCross1 } from "react-icons/rx";
+
+const menuItems = [
+  { title: "Home", link: "/" },
+  { title: "About", link: "/about" },
+  { title: "Contact", link: "/contact" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="flex justify-between items-center px-6 sm:px-10 md:px-14 lg:px-18 py-2 shadow-md">
+      {/* ----------------- Logo and Title ----------------  */}
+      <div className="flex items-center">
+        <img src={logo} className="h-15" alt="logo" />
+        <h1 className="font-bold font-display text-md sm:text-lg md:text-xl lg:text-2xl text-secondary ml-3">UFC GYM</h1>
+      </div>
+      {/* ----------------- Menu Items for Desktop ----------------  */}
+      <div className="hidden md:flex items-center gap-6 ">
+        <ul className="flex w-full gap-6">
+          {menuItems.map((item) => (
+            <li className="px-3 sm:text-sm md:text-md lg:text-xl text-secondary hover:text-primary transition duration-500  font-medium"><Link to={item.link}>{item.title}</Link></li>
+          ))}
+        </ul>
+          {/* ----------------- Call to Action ----------------  */}
+        <button className="px-4 sm:px-6 md:px-8 lg:px-10 py-2 bg-secondary hover:bg-primary transition duration-500 text-white rounded-full w-full  ">
+          <Link to="/" className="">Join Us</Link>
+        </button>
+      </div>
+
+      {/* ----------------- Menu Items for Mobile ----------------  */}
+      <div className="md:hidden flex">
+        <CiMenuFries onClick={() => setOpen(!open)} className="text-3xl text-secondary cursor-pointer" />
+
+        <div className={`${open ? "translate-y-0" : "-translate-x-full"} absolute top-0 left-0 w-full h-screen bg-white flex flex-col justify-center gap-12 items-center transition-all duration-300 `}>
+        <RxCross1 onClick={() => setOpen(!open)} className="text-3xl absolute top-8 right-4 text-secondary cursor-pointer" />
+
+        <ul className="flex flex-col  gap-6">
+          {menuItems.map((item) => (
+            <li className="px-3 font-bold  text-secondary hover:text-primary transition duration-500"><Link to={item.link}>{item.title}</Link></li>
+          ))}
+        </ul>
+          {/* ----------------- Call to Action ----------------  */}
+        <button className="px-4 sm:px-6 md:px-8 lg:px-10 py-2 bg-secondary hover:bg-primary transition duration-500 text-white rounded-full w-[70%]  ">
+          <Link to="/" className="">Join Us</Link>
+        </button>
+        </div>
+          
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
