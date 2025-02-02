3fa3a1745903ddb6c99638c26f2390e16c2c4aed
ankitchoubeyy <imkitchoubey@gmail.com>
2025-02-03 01:02:11 +0530

    stats section added

---
+++ client/src/components/StatsSection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const stats = [
+  { id: 1, title: "Years of Service", value: "7+" },
+  { id: 2, title: "Certified Trainers", value: "5+" },
+  { id: 3, title: "Happy Members", value: "786+" },
+  { id: 4, title: "Customer Satisfaction", value: "95%" },
+];
+
+const StatsSection = () => {
+  return (
+    <div className="flex justify-between items-center mt-10">
+      {stats.map((stat) => (
+        <div className="text-center">
+          <h1 className="text-3xl font-bold mb-1">{stat.value}</h1>
+          <p className="text-lg ">{stat.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default StatsSection;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import StatsSection from '../components/StatsSection'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      
     </div>
   )
 }
